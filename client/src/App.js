974c5167824a27cc89df74015006d5bc0662564b
alexandreoliv <alexandrebo@gmail.com>
2021-07-23 01:30:00 +0200

    change Mapbox to npm

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
 import './App.css';
 import TravelList from './components/travels/TravelList';
+import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+mapboxgl.accessToken = 'pk.eyJ1IjoiYWxleGFuZHJlb2xpdiIsImEiOiJja3FtYTIxYm4wMHJkMnVtaGMyZ2t2dmF0In0.AVesP7QNz89Q7ND7AF2ikQ';
 
 class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			lat: 13.4050,
+			lng: 52.5200,
+			zoom: 9
+		};
+		this.mapContainer = React.createRef();
+	}
+
+	componentDidMount() {
+		const { lat, lng, zoom } = this.state;
+		const map = new mapboxgl.Map({
+		  container: this.mapContainer.current,
+		  style: 'mapbox://styles/mapbox/streets-v11',
+		  center: [lat, lng],
+		  zoom: zoom
+		});
+	  }
+
 	render() {
 		return (
 			<div className="App">
+				<div ref={this.mapContainer} className="map-container" />
 				<TravelList />
 			</div>
 		)
